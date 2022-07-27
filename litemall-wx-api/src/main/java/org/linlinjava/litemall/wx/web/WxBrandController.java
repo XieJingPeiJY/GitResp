6e15d30c0bbe52a35f6d063c74362e75d2f374fb
xiejingpei <932258023@qq.com>
2022-07-27 22:04:39 +0800

    dev测试66666

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxBrandController.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxBrandController.java
@@ -45,7 +45,7 @@ public class WxBrandController {
                        @Order @RequestParam(defaultValue = "desc") String order) {
         List<LitemallBrand> brandList = brandService.query(page, limit, sort, order);
         LitemallBrand litemallBrand = new LitemallBrand();
-        litemallBrand.setDesc("pppppppppppppppppppppppppppjjjjjjjjjjjjjjjjjjjjjjjjjjjj888");
+        litemallBrand.setDesc("ppppppppppppppppppppp66ppppppjjjjjjjjjjjjjjjjjjjjjjjjjjjj888");
 
         brandList.add(litemallBrand);
         return ResponseUtil.okList(brandList);
